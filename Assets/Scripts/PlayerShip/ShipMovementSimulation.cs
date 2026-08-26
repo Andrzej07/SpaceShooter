a2254de8e8ea91b399e0ebabe33d59a8884dbd7f
Maciej Olszowy <maciej.olszowy@op.pl>
2026-08-26 12:13:33 +0200

    added bounds to movement

--- Assets/Scripts/PlayerShip/ShipMovementSimulation.cs
+++ Assets/Scripts/PlayerShip/ShipMovementSimulation.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Unity.Mathematics;
+using UnityEngine;
 
 namespace SpaceShooter
 {
@@ -8,13 +9,33 @@ namespace SpaceShooter
 		private new Rigidbody2D rigidbody;
 		[SerializeField]
 		private float speed;
+		[SerializeField]
+		private Rect bounds;
 		
 		protected override void OnInitialize() { }
 		
 		public override void TickSimulation()
 		{
 			var positionDelta = Ship.LatestInput.move * speed * Time.fixedDeltaTime;
-			rigidbody.MovePosition(rigidbody.position + positionDelta);
+
+			if (positionDelta == Vector2.zero)
+			{
+				return;
+			}
+			
+			Vector2 oldPosition = rigidbody.position;
+			Vector2 newPosition = oldPosition + positionDelta;
+			newPosition = Clamp(newPosition);
+
+			if (newPosition != oldPosition)
+			{
+				rigidbody.MovePosition(newPosition);
+			}
+		}
+		
+		private Vector2 Clamp(Vector2 newPosition)
+		{
+			return math.clamp(newPosition, bounds.min, bounds.max);
 		}
 	}
 }
